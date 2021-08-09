a752efe02bed1736a91d0901c33a9e40c79ceeb8
chellkamp <chellkamp@yahoo.com>
2021-08-09 14:14:31 -0400

    Commit changes that weren't added with last commit.

--- app/src/main/java/com/codingwithoutpants/blackholesavior/MainActivity.java
+++ app/src/main/java/com/codingwithoutpants/blackholesavior/MainActivity.java
@@ -1,33 +1,76 @@
 package com.codingwithoutpants.blackholesavior;
 
 import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.RectF;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.codingwithoutpants.gamelib.activity.FixedResPortraitActivity;
+import com.codingwithoutpants.gamelib.sprite.Meteor;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends FixedResPortraitActivity {
 
-    private Bitmap _splashBitmap;
+    //OLD CODE
+    //private Bitmap _splashBitmap;
+
+    private float _screenWidth;
+    private float _screenHeight;
+
+    private Bitmap _meteorBitmap;
+
+    // Range of valid center points we can use when we create a meteor sprite off screen
+    private RectF _meteorSpawnRange;
+    private RectF _meteorDirVecRange; // direction vector range
 
     @Override
     protected void init() {
         super.init();
 
-        AssetManager assetMgr = getAssets();
 
         try {
-            // load the splash image asset into memory
-            InputStream is = assetMgr.open("img/titleart.png");
-            _splashBitmap = BitmapFactory.decodeStream(is, null, null);
+            Resources res = getResources();
+            _screenWidth =
+                    (float) res.getInteger(com.codingwithoutpants.gamelib.R.integer.portraitWidthPx);
+            _screenHeight =
+                    (float) res.getInteger(com.codingwithoutpants.gamelib.R.integer.portraitHeightPx);
+
+            _meteorSpawnRange = new RectF(
+                    Meteor.RADIUS, -2f * Meteor.RADIUS,
+                    _screenWidth - Meteor.RADIUS, -2f * Meteor.RADIUS
+            );
+
+            _meteorDirVecRange = new RectF(
+                    _meteorSpawnRange.left, _meteorSpawnRange.top + _meteorSpawnRange.width(),
+                    _meteorSpawnRange.right, _meteorSpawnRange.top + _meteorSpawnRange.width()
+            );
+
+            AssetManager assetMgr = getAssets();
+
+            //OLD CODE
+            //// load the splash image asset into memory
+            //InputStream is = assetMgr.open("img/titleart.png");
+            //_splashBitmap = BitmapFactory.decodeStream(is, null, null);
+            //try { is.close(); } catch (IOException closeEx) { /* do nothing.  We tried. */}
+
+
+            InputStream is;
+            is = assetMgr.open("img/meteor.png");
+
+            _meteorBitmap = BitmapFactory.decodeStream(is, null, null);
             try { is.close(); } catch (IOException closeEx) { /* do nothing.  We tried. */}
+
+
+
+            _meteorSpawnRange = new RectF();
+
         } catch(Exception ex) {
             // I can't do anything from here but log the error, then roll over and die.
             Log.e("init", "Error initializing activity.", ex);
@@ -43,9 +86,10 @@ public class MainActivity extends FixedResPortraitActivity {
 
     @Override
     protected void draw(@NonNull Canvas c) {
-        // draw the splash image with top-left corner at (0, 0) of the canvas.
-        // the bitmap is 1080 x 1920, so it should fit the fixed size of the canvas
-        // perfectly.
-        c.drawBitmap(_splashBitmap, 0f, 0f, null);
+        //OLD CODE
+        //// draw the splash image with top-left corner at (0, 0) of the canvas.
+        //// the bitmap is 1080 x 1920, so it should fit the fixed size of the canvas
+        //// perfectly.
+        //c.drawBitmap(_splashBitmap, 0f, 0f, null);
     }
 }
--- gamelib/src/main/java/com/codingwithoutpants/gamelib/sprite/Meteor.java
+++ gamelib/src/main/java/com/codingwithoutpants/gamelib/sprite/Meteor.java
@@ -1,5 +1,6 @@
 package com.codingwithoutpants.gamelib.sprite;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.PointF;
@@ -10,22 +11,120 @@ import androidx.annotation.NonNull;
  * Meteor class, duh
  */
 public class Meteor {
-    private PointF _origin = new PointF();
-    private float _angle;
+    private static float _IMG_DIMENSION = 100f;
+    private static PointF _IMG_CENTER = new PointF(50f, 50f);
 
-    private PointF _velocity = new PointF();
-    private float _rotVelocity; // rotational velocity
+    public final static float RADIUS = 45f;
 
-    private PointF _acceleration = new PointF();
-    private float _rotAcceleration; // rotational acceleration
+    private Bitmap _bitmap;
+
+    private PointF _center = new PointF();
+    private float _angleDegrees;
+
+    private PointF _velocity = new PointF(); // measured in pixels per nanosecond
+    private float _rotVelocity; // measured in degrees per nanosecond
+
+    private PointF _acceleration = new PointF(); // measured in pixels per nanosecond per nanosecond
+    private float _rotAcceleration; // measured in degrees per nanosecond per nanosecond
 
     private Matrix _drawMatrix = new Matrix(); // used in the draw() method
 
-    public void update() {
+    /**
+     * Constructor
+     * @param bitmap meteor image
+     */
+    public Meteor(@NonNull Bitmap bitmap) {
+        _bitmap = bitmap;
+    }
 
+    /**
+     * Position the meteor by its center
+     * @param x x-coordinate
+     * @param y y-coordinate
+     */
+    public void setCenter(float x, float y) {
+        _center.set(x, y);
+    }
+
+    /**
+     * Set the angle
+     * @param degrees angle, in degrees
+     */
+    public void setAngle(float degrees) {
+        // normalize degrees to [0, 360) before saving to this object
+        if (degrees < 0f) {
+            degrees = 360f - (-degrees % 360f);
+        }
+        degrees = degrees % 360f;
+
+        _angleDegrees = degrees;
+    }
+
+    /**
+     * Sets the traveling velocity of this object
+     * @param dXPerNs X-axis change in pixels per nanosecond
+     * @param dYPerNs Y-axis change in pixels per nanosecond
+     */
+    public void setVelocity(float dXPerNs, float dYPerNs) {
+        _velocity.set(dXPerNs, dYPerNs);
+    }
+
+    /**
+     * Set the rotational velocity
+     * @param degPerNs degrees per nanosecond
+     */
+    public void setRotationalVelocity(float degPerNs) {
+        _rotVelocity = degPerNs;
+    }
+
+    /**
+     * Set the acceleration vector
+     * @param dXPerNsPerNs X-axis change in pixels per nanosecond per nanosecond
+     * @param dYPerNsPerNs Y-axis change in pixels per nanosecond per nanosecond
+     */
+    public void setAcceleration(float dXPerNsPerNs, float dYPerNsPerNs) {
+        _acceleration.set(dXPerNsPerNs, dYPerNsPerNs);
+    }
+
+    /**
+     * Set the rotational acceleration.
+     * @param degPerNsPerNs degrees per nanosecond per nanosecond
+     */
+    public void setRotationalAcceleration(float degPerNsPerNs) {
+        _rotAcceleration = degPerNsPerNs;
+    }
+
+    /**
+     * Update the object
+     * @param elapsedNs elapsed time, in nanosec
+     */
+    public void update(long elapsedNs) {
+
+        // update velocities based on their accelerations...
+
+        _velocity.x += _acceleration.x * elapsedNs;
+        _velocity.y += _acceleration.y * elapsedNs;
+
+        _rotVelocity += _rotAcceleration * elapsedNs;
+
+        // Now, update positioning based on velocities...
+
+        _center.x += _velocity.x * elapsedNs;
+        _center.y += _velocity.y * elapsedNs;
+
+        // use setAngle() because it'll normalize the result
+        setAngle(_angleDegrees + _rotVelocity * elapsedNs);
     }
 
     public void draw(@NonNull Canvas c) {
-        _
+        // The set...() functions on the Matrix class clear out all previous operations in the
+        // Matrix before applying the new operation.
+        _drawMatrix.setRotate(_angleDegrees, _IMG_CENTER.x, _IMG_CENTER.y);
+
+        // Notice this is a postTranslate() and not a preTranslate().  preTranslate() would've
+        // applied the translation BEFORE the rotation around the image's center point.
+        _drawMatrix.postTranslate(_center.x - _IMG_CENTER.x, _center.y - _IMG_CENTER.y);
+
+        c.drawBitmap(_bitmap, _drawMatrix, null);
     }
 }
